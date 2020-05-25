2eefd71106735e7bb91abde8b3c9efbcffa4b333
ClemV <22980442+clementviolet@users.noreply.github.com>
2020-05-25 16:04:37 +0200

    Render other formats locally

--- local.compilation.sh
+++ local.compilation.sh
@@ -20,7 +20,18 @@ cp -r figures rendered/
 mv references.json rendered/
 
 # Create tex file
+echo "TeX document"
 pandoc manuscript.md -s -o rendered/$repo.tex --filter pandoc-xnos --bibliography=./rendered/references.json --metadata-file=metadata.json --template=.assets/templates/template.tex
+
+# Create html file
+echo "HTML document"
+mkdir -p rendered/css/
+cp .assets/templates/style.less rendered/css/
+pandoc manuscript.md -o rendered/index.html --filter pandoc-xnos --template=.assets/templates/template.html --bibliography=./rendered/references.json --metadata-file=metadata.json --webtex
+
+# Create docx file
+echo "MS Word document"
+pandoc manuscript.md -s -o rendered/$repo.docx --filter pandoc-xnos --bibliography=./rendered/references.json --metadata-file=metadata.json
 
 # Fix the issue with longtable package
 
@@ -28,6 +39,7 @@ python .assets/scripts/longtablefix.py
 
 # Create pdf file
 cd rendered
+echo "PDF Document"
 latexmk $repo.tex -lualatex --file-line-error --interaction=nonstopmode
 
 # Cleaning up
